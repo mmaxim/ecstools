24f60476432a5b588531ad40fb1a3ec891398c0e
Mike Maxim <mike@keyba.se>
2016-12-18 23:09:09 -0500

    somewhat functional!

--- bin/ecslist/main.go
+++ bin/ecslist/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
+	"text/tabwriter"
+
+	"strings"
 
 	"github.com/mmaxim/ecstools/libecs"
+	"github.com/reconquest/loreley"
 )
 
 func main() {
@@ -13,14 +18,27 @@ func main() {
 	os.Exit(rc)
 }
 
+func truncateARN(arn string, enabled bool) string {
+	if !enabled {
+		return arn
+	}
+	toks := strings.Split(arn, "/")
+	return toks[1]
+}
+
 func mainInner() int {
 	var clusterName, region string
+	var shortArns bool
 
 	flag.StringVar(&clusterName, "cluster", "cluster", "cluster name")
 	flag.StringVar(&region, "region", "us-east-1", "AWS region name")
+	flag.BoolVar(&shortArns, "short-arns", true, "display only last part of ARN")
 	flag.Parse()
 
-	ecs, err := libecs.New(clusterName, region)
+	ecs, err := libecs.New(libecs.ECSConfig{
+		Cluster: clusterName,
+		Region:  region,
+	})
 	if err != nil {
 		fmt.Printf("failed to create ECS API object: %s", err.Error())
 		return 3
@@ -31,9 +49,36 @@ func mainInner() int {
 		fmt.Printf("failed to list services: %s", err.Error())
 	}
 
+	buffer := &bytes.Buffer{}
+	w := tabwriter.NewWriter(buffer, 0, 3, 5, ' ', tabwriter.FilterHTML)
+	fmt.Fprintf(w, "<fg 13><bold>Name\tRunning\tPending\tTask\tCPU%%\tMemory%%<reset>\n")
 	for _, s := range services {
-		fmt.Printf("%s %s\n", s.Name, s.Arn)
+		fmt.Fprintf(w, "%s\t%d\t%d\t%s\t%f\t%f\n", s.Name, s.RunningCount, s.PendingCount,
+			truncateARN(s.TaskDefinition, shortArns), s.Metrics.CPU, s.Metrics.Memory)
 	}
+	w.Flush()
+	fmt.Fprintf(w, "\n")
+	w.Flush()
+
+	fmt.Fprintf(w, "<fg 13><bold>Service\tStatus\tTask\tInstance ID\tInstance CPU%%<reset>\n")
+	for _, svc := range services {
+		for _, task := range svc.Tasks {
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%f%%\n", svc.Name, task.Status,
+				truncateARN(task.TaskDefinition, shortArns), task.InstanceMetrics.ID,
+				task.InstanceMetrics.CPU)
+		}
+	}
+	w.Flush()
+
+	loreley.DelimLeft = "<"
+	loreley.DelimRight = ">"
+	result, err := loreley.CompileAndExecuteToString(buffer.String(), nil, nil)
+	if err != nil {
+		fmt.Printf("error formating output: %s", err.Error())
+		return 3
+	}
+
+	fmt.Print(result)
 
 	return 0
 }
--- libecs/ecs.go
+++ libecs/ecs.go
@@ -1,15 +1,31 @@
 package libecs
 
 import (
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/ecs"
 )
 
+type ECSConfig struct {
+	Cluster string
+	Region  string
+}
+
 type ECS struct {
-	ecs     *ecs.ECS
-	cluster string
-	region  string
+	ecs        *ecs.ECS
+	cloudwatch *cloudwatch.CloudWatch
+	config     ECSConfig
+}
+
+type Task struct {
+	Arn             string
+	InstanceArn     string
+	Status          string
+	TaskDefinition  string
+	InstanceMetrics InstanceMetrics
 }
 
 type Service struct {
@@ -18,38 +34,205 @@ type Service struct {
 	RunningCount   int
 	PendingCount   int
 	TaskDefinition string
+	Tasks          []Task
+	Metrics        ServiceMetrics
 }
 
-func New(clusterName string, region string) (*ECS, error) {
+type InstanceMetrics struct {
+	CPU float64
+	ID  string
+}
+
+type ServiceMetrics struct {
+	CPU    float64
+	Memory float64
+}
+
+func New(config ECSConfig) (*ECS, error) {
 	ret := &ECS{
-		cluster: clusterName,
-		region:  region,
+		config: config,
 	}
 
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(config.Region)})
 	if err != nil {
 		return nil, err
 	}
 	ret.ecs = ecs.New(sess)
+	ret.cloudwatch = cloudwatch.New(sess)
 
 	return ret, nil
 }
 
-func (e *ECS) ListServices() ([]Service, error) {
-	params := &ecs.DescribeClustersInput{
-		Clusters: []*string{aws.String(e.cluster)},
+func (e *ECS) cluster() string {
+	return e.config.Cluster
+}
+
+func (e *ECS) region() string {
+	return e.config.Region
+}
+
+func (e *ECS) getInstanceMetrics(instance string) (InstanceMetrics, error) {
+	resp, err := e.ecs.DescribeContainerInstances(&ecs.DescribeContainerInstancesInput{
+		Cluster:            aws.String(e.cluster()),
+		ContainerInstances: []*string{aws.String(instance)},
+	})
+	if err != nil {
+		return InstanceMetrics{}, err
 	}
-	resp, err := e.ecs.DescribeClusters(params)
+	id := resp.ContainerInstances[0].Ec2InstanceId
+	dim := cloudwatch.Dimension{
+		Name:  aws.String("InstanceId"),
+		Value: id,
+	}
+	period := int64(60)
+	start := time.Now().Add(-2 * time.Minute)
+	end := time.Now()
+	cpuResp, err := e.cloudwatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+		Dimensions: []*cloudwatch.Dimension{&dim},
+		MetricName: aws.String("CPUUtilization"),
+		Statistics: []*string{aws.String("Average")},
+		Period:     &period,
+		StartTime:  &start,
+		EndTime:    &end,
+		Namespace:  aws.String("AWS/EC2"),
+	})
+	if err != nil {
+		return InstanceMetrics{}, err
+	}
+
+	cpu := cpuResp.Datapoints[0].Average
+	return InstanceMetrics{
+		CPU: *cpu,
+		ID:  aws.StringValue(id),
+	}, nil
+}
+
+func (e *ECS) listTasks(svc *ecs.Service) ([]Task, error) {
+	resp, err := e.ecs.ListTasks(&ecs.ListTasksInput{
+		Cluster:     aws.String(e.cluster()),
+		ServiceName: svc.ServiceName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	respt, err := e.ecs.DescribeTasks(&ecs.DescribeTasksInput{
+		Cluster: aws.String(e.cluster()),
+		Tasks:   resp.TaskArns,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var res []Task
+	for _, t := range respt.Tasks {
+		im, err := e.getInstanceMetrics(aws.StringValue(t.ContainerInstanceArn))
+		if err != nil {
+			return res, err
+		}
+		res = append(res, Task{
+			Arn:             aws.StringValue(t.TaskArn),
+			InstanceArn:     aws.StringValue(t.ContainerInstanceArn),
+			Status:          aws.StringValue(t.LastStatus),
+			TaskDefinition:  aws.StringValue(t.TaskDefinitionArn),
+			InstanceMetrics: im,
+		})
+	}
+
+	return res, nil
+}
+
+func (e *ECS) getServiceMetrics(svcname string) (ServiceMetrics, error) {
+	dims := []*cloudwatch.Dimension{
+		&cloudwatch.Dimension{
+			Name:  aws.String("ClusterName"),
+			Value: aws.String(e.cluster()),
+		},
+		&cloudwatch.Dimension{
+			Name:  aws.String("ServiceName"),
+			Value: aws.String(svcname),
+		},
+	}
+	period := int64(60)
+	start := time.Now().Add(-2 * time.Minute)
+	end := time.Now()
+	cpuResp, err := e.cloudwatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+		Dimensions: dims,
+		MetricName: aws.String("CPUUtilization"),
+		Statistics: []*string{aws.String("Average")},
+		Period:     &period,
+		StartTime:  &start,
+		EndTime:    &end,
+		Namespace:  aws.String("AWS/ECS"),
+	})
+	if err != nil {
+		return ServiceMetrics{}, err
+	}
+
+	memResp, err := e.cloudwatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+		Dimensions: dims,
+		MetricName: aws.String("MemoryUtilization"),
+		Statistics: []*string{aws.String("Average")},
+		Period:     &period,
+		StartTime:  &start,
+		EndTime:    &end,
+		Namespace:  aws.String("AWS/ECS"),
+	})
+	if err != nil {
+		return ServiceMetrics{}, err
+	}
+
+	cpu := cpuResp.Datapoints[0].Average
+	mem := memResp.Datapoints[0].Average
+	return ServiceMetrics{
+		CPU:    *cpu,
+		Memory: *mem,
+	}, nil
+}
+
+func (e *ECS) ListServices() ([]Service, error) {
+
+	// Fetch all service ARNS
+	params := &ecs.ListServicesInput{
+		Cluster:    aws.String(e.cluster()),
+		MaxResults: aws.Int64(100),
+	}
+	resp, err := e.ecs.ListServices(params)
+	if err != nil {
+		return nil, err
+	}
+	var arns []*string
+	for _, a := range resp.ServiceArns {
+		arns = append(arns, a)
+	}
+
+	// Fetch service descriptions
+	sresp, err := e.ecs.DescribeServices(&ecs.DescribeServicesInput{
+		Services: arns,
+		Cluster:  aws.String(e.cluster()),
+	})
 	if err != nil {
 		return nil, err
 	}
 
 	var res []Service
-	for _, c := range resp.Clusters {
-		res = append(res, Service{
-			Name: *c.ClusterName,
-			Arn:  *c.ClusterArn,
-		})
+	for _, svc := range sresp.Services {
+		metrics, err := e.getServiceMetrics(aws.StringValue(svc.ServiceName))
+		if err != nil {
+			return nil, err
+		}
+		s := Service{
+			Name:           aws.StringValue(svc.ServiceName),
+			Arn:            aws.StringValue(svc.ServiceArn),
+			RunningCount:   int(aws.Int64Value(svc.RunningCount)),
+			PendingCount:   int(aws.Int64Value(svc.PendingCount)),
+			TaskDefinition: aws.StringValue(svc.TaskDefinition),
+			Metrics:        metrics,
+		}
+		if s.Tasks, err = e.listTasks(svc); err != nil {
+			return nil, err
+		}
+		res = append(res, s)
 	}
 
 	return res, nil
