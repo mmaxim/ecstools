2379b9a4d9e9b6cc789bdd93551cb7bec0555cef
Mike Maxim <mike.maxim@zoom.us>
2025-02-20 12:30:51 -0500

    update

--- bin/ecstop/main.go
+++ bin/ecstop/main.go
@@ -7,8 +7,10 @@ import (
 	"os"
 	"strings"
 	"text/tabwriter"
+	"time"
 
-	ui "github.com/gizak/termui"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"github.com/mmaxim/ecstools/libecs"
 	"github.com/reconquest/loreley"
 )
@@ -24,6 +26,20 @@ func truncateARN(arn string, enabled bool) string {
 	}
 	toks := strings.Split(arn, "/")
 	return toks[1]
+}
+
+func getInstanceID(task libecs.Task) string {
+	if task.InstanceMetrics != nil {
+		return task.InstanceMetrics.ID
+	}
+	return "n/a"
+}
+
+func getInstanceCPU(task libecs.Task) string {
+	if task.InstanceMetrics != nil {
+		return fmt.Sprintf("%f%%", task.InstanceMetrics.CPU)
+	}
+	return "n/a"
 }
 
 func renderSvcs(ecs *libecs.ECS) (string, string) {
@@ -51,15 +67,37 @@ func renderSvcs(ecs *libecs.ECS) (string, string) {
 	for _, svc := range services {
 		for _, task := range svc.Tasks {
 			ca := task.CreatedAt.Format("01-02-2006 15:04")
-			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%f%%\n", svc.Name, task.Status,
-				truncateARN(task.TaskDefinition, true), ca, task.InstanceMetrics.ID,
-				task.InstanceMetrics.CPU)
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n", svc.Name, task.Status,
+				truncateARN(task.TaskDefinition, true), ca, getInstanceID(task),
+				getInstanceCPU(task))
 		}
 	}
 	w.Flush()
 	taskResult, _ := loreley.CompileAndExecuteToString(buffer.String(), nil, nil)
 
 	return svcResult, taskResult
+}
+
+type workerRes struct {
+	svcRes  string
+	taskRes string
+}
+
+func startRefreshWorker(workCh chan struct{}, ecs *libecs.ECS, svcui *widgets.Paragraph) (resCh chan workerRes) {
+	resCh = make(chan workerRes, 1)
+	go func() {
+		for range workCh {
+			var res workerRes
+			svcui.Title = "Services (refreshing)"
+			ui.Render(svcui)
+			res.svcRes, res.taskRes = renderSvcs(ecs)
+			svcui.Title = "Services"
+			ui.Render(svcui)
+			resCh <- res
+		}
+	}()
+	workCh <- struct{}{}
+	return resCh
 }
 
 func mainInner() int {
@@ -85,66 +123,58 @@ func mainInner() int {
 		return 3
 	}
 
-	svcRes, taskRes := renderSvcs(ecs)
-	svcui := ui.NewPar(svcRes)
-	svcui.Height = 7
+	workCh := make(chan struct{}, 1)
+	width := 120
+
+	svcui := widgets.NewParagraph()
 	svcui.PaddingLeft = 3
 	svcui.PaddingBottom = 1
 	svcui.PaddingTop = 1
-	svcui.BorderLabel = "Services"
-	svcui.BorderFg = ui.ColorYellow
+	svcui.Title = "Services"
+	svcui.SetRect(0, 0, width, 40)
+	svcui.TitleStyle.Fg = ui.ColorYellow
 
-	taskui := ui.NewPar(taskRes)
-	taskui.Height = 8
+	resCh := startRefreshWorker(workCh, ecs, svcui)
+
+	taskui := widgets.NewParagraph()
 	taskui.PaddingLeft = 3
 	taskui.PaddingBottom = 1
 	taskui.PaddingTop = 1
-	taskui.BorderLabel = "Tasks"
-	taskui.BorderFg = ui.ColorYellow
+	taskui.Title = "Tasks"
+	taskui.TitleStyle.Fg = ui.ColorYellow
+	taskui.SetRect(0, 40, width, 80)
 
-	g := ui.NewGauge()
+	g := widgets.NewGauge()
 	g.Percent = 0
-	g.Height = 3
-	g.Y = 11
-	g.BorderLabel = "Refresh"
+	g.Title = "Refresh"
 	g.BarColor = ui.ColorRed
-	g.BorderFg = ui.ColorWhite
-	g.BorderLabelFg = ui.ColorCyan
+	g.TitleStyle.Fg = ui.ColorWhite
+	g.BorderStyle.Fg = ui.ColorCyan
+	g.SetRect(0, 80, width, 85)
 
-	// build
-	ui.Body.AddRows(
-		ui.NewRow(
-			ui.NewCol(12, 0, g),
-		),
-		ui.NewRow(
-			ui.NewCol(12, 0, svcui),
-		),
-		ui.NewRow(
-			ui.NewCol(12, 0, taskui),
-		),
-	)
+	ui.Render(svcui, taskui, g)
 
-	// calculate layout
-	ui.Body.Align()
-
-	ui.Render(ui.Body)
-
-	ui.Handle("/sys/kbd/q", func(ui.Event) {
-		ui.StopLoop()
-	})
-	ui.Handle("/timer/1s", func(e ui.Event) {
-
-		t := e.Data.(ui.EvtTimer)
-		g.Percent = int((float64(t.Count%10) / 10.0) * 100)
-		if t.Count%10 == 0 {
-			svcres, taskres := renderSvcs(ecs)
-			svcui.Text = svcres
-			taskui.Text = taskres
+	uiEvents := ui.PollEvents()
+	count := 0
+	refreshLen := 20
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return 0
+			}
+		case res := <-resCh:
+			svcui.Text = res.svcRes
+			taskui.Text = res.taskRes
+			ui.Render(svcui, taskui)
+		case <-time.After(time.Second):
+			g.Percent = int((float64(count%refreshLen) / float64(refreshLen)) * 100)
+			if count%10 == 0 {
+				workCh <- struct{}{}
+			}
+			count++
+			ui.Render(g)
 		}
-		ui.Render(ui.Body)
-	})
-
-	ui.Loop()
-
-	return 0
+	}
 }
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/mmaxim/ecstools
+
+go 1.24.0
+
+require (
+	github.com/aws/aws-sdk-go v1.55.6
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/keybase/go-keybase-chat-bot v0.0.0-20250106203511-859265729a56
+	github.com/reconquest/loreley v0.0.0-20211011075601-29b1d7b0ad91
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
